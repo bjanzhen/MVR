176f0485ba626f344c6e0b6120e6d8483738885f
ljzyal <ljzyal@gmail.com>
2022-11-15 01:39:42 -0800

    Updates

---
+++ precache-manifest.5cbaa94ee73fc662aa7ead9d8cea9e1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "285e3bad4057e0db94cc",
+    "url": "/MVR/static/js/runtime~main.6ccdad58.js"
+  },
+  {
+    "revision": "bb48f5973d63924b7a9f",
+    "url": "/MVR/static/js/main.64284950.chunk.js"
+  },
+  {
+    "revision": "a40a2af186eb990d2de3",
+    "url": "/MVR/static/js/2.9ec3f89c.chunk.js"
+  },
+  {
+    "revision": "bb48f5973d63924b7a9f",
+    "url": "/MVR/static/css/main.e4583e07.chunk.css"
+  },
+  {
+    "revision": "a40a2af186eb990d2de3",
+    "url": "/MVR/static/css/2.a0cd9e68.chunk.css"
+  },
+  {
+    "revision": "ba89df2d5ea2dd67af035d8421fe797e",
+    "url": "/MVR/index.html"
+  }
+];
